ba3ea7dc0b07e6d96f1c3221b9bc7a5ad71f8ca2
Viktoriia Kamorina <48948022+VictoriaKam@users.noreply.github.com>
2020-03-02 11:55:03 +0200

    feat: add solution for cats-function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,12 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+        let cats = 0;
+	for (let i=0; i < backyard.length; i++) {
+		for (let m=0; m < backyard[i].length; m++) {
+			if (backyard[i][m] === '^^') {
+				cats++;
+			}
+		};
+	}
+	return cats;
+			
 };
