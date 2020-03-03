0cae4ae4ce2ffbc94a6c249368faf3ccab94fab3
Viktoriia Kamorina <48948022+VictoriaKam@users.noreply.github.com>
2020-03-03 13:14:58 +0200

    feat: add solutions for dream-team task and what-season-task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,17 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+function getClass(obj) {
+	return {}.toString.call(obj).slice(8, -1);
+}
+
+module.exports = function createDreamTeam(array) {
+  if (getClass(array) === "Array") {
+	  let team = [];
+	  for (let i = 0; i < array.length; i++) {
+		  if (getClass(array[i]) === "String") {
+			  team.push(array[i].trim()[0].toUpperCase());
+		  }
+	  }
+	  return team.sort().join('');
+  } else {
+	  return false;
+  }
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,23 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+function getClass(obj) {
+	return {}.toString.call(obj).slice(8, -1);
+}
+
+module.exports = function getSeason(date) {
+  if (typeof(date) === 'undefined') {
+	  return "Unable to determine the time of year!";
+  } else if (getClass(date) === "Date") {
+	  let month = date.getMonth();
+	  if (month === 11 || month === 0 || month === 1) {
+		  return 'winter';
+	  } else if (month === 2 || month === 3 || month === 4) {
+		  return 'spring';
+	  } else if (month === 5 || month === 6 || month === 7) {
+		  return 'summer';
+	  } else {
+		  return 'autumn';
+	  }
+  } else {
+	  throw new Error();
+  }
+  
 };
